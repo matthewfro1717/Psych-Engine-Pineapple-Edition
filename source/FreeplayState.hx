228b80a3761d26d447d8ed11157de2c9bd63bfc5
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2022-11-01 21:43:42 -0400

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -28,7 +28,7 @@ class FreeplayState extends MusicBeatState
 	var curSelected:Int = 0;
 	var curDifficulty:Int = 1;
 
-	var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('backgrounds/SUSSUS AMOGUS'));
+	var bg:FlxSprite;
 
 	var scoreBG:FlxSprite;
 	var scoreText:FlxText;
